8cbf046dd285f52c54c45b3d66173cbbb29ef805
Patryk <patryk.korzeniowski3@gmail.com>
2020-09-01 22:35:56 +0200

    ustawione kontrolki noty ksiegowej

--- FakturoPol/NowaNotaKsiegowa.Designer.cs
+++ FakturoPol/NowaNotaKsiegowa.Designer.cs
@@ -28,32 +28,365 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
+            this.oplacono_label = new System.Windows.Forms.Label();
+            this.metodaPlatnosci_label = new System.Windows.Forms.Label();
+            this.Opis_label = new System.Windows.Forms.Label();
+            this.razem_label = new System.Windows.Forms.Label();
+            this.cena_label = new System.Windows.Forms.Label();
+            this.lp_label = new System.Windows.Forms.Label();
+            this.dataWystawienia_label = new System.Windows.Forms.Label();
+            this.Seria_label = new System.Windows.Forms.Label();
+            this.Nabywca_label = new System.Windows.Forms.Label();
+            this.terminPlatnosci_label = new System.Windows.Forms.Label();
+            this.tresc_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.oplacono_textBox = new System.Windows.Forms.TextBox();
+            this.terminPlatnosci_comboBox = new System.Windows.Forms.ComboBox();
+            this.dodajPozycje_button = new System.Windows.Forms.Button();
+            this.DataWystawienia_dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.nabywca_comboBox = new System.Windows.Forms.ComboBox();
+            this.seria_comboBox = new System.Windows.Forms.ComboBox();
+            this.metodaPlatnosci_comboBox = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LPPozycjaNoty_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.pozycjaNoty_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.razem_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.anuluj_button = new System.Windows.Forms.Button();
+            this.zapisz_button = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.załącznik_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
-            // label2
+            // oplacono_label
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(284, 209);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(180, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Zaczekaj chwile. Ładuje się";
+            this.oplacono_label.AutoSize = true;
+            this.oplacono_label.Location = new System.Drawing.Point(492, 133);
+            this.oplacono_label.Name = "oplacono_label";
+            this.oplacono_label.Size = new System.Drawing.Size(69, 17);
+            this.oplacono_label.TabIndex = 3;
+            this.oplacono_label.Text = "Opłacono";
+            // 
+            // metodaPlatnosci_label
+            // 
+            this.metodaPlatnosci_label.AutoSize = true;
+            this.metodaPlatnosci_label.Location = new System.Drawing.Point(325, 133);
+            this.metodaPlatnosci_label.Name = "metodaPlatnosci_label";
+            this.metodaPlatnosci_label.Size = new System.Drawing.Size(115, 17);
+            this.metodaPlatnosci_label.TabIndex = 4;
+            this.metodaPlatnosci_label.Text = "Metoda płatności";
+            // 
+            // Opis_label
+            // 
+            this.Opis_label.AutoSize = true;
+            this.Opis_label.Location = new System.Drawing.Point(108, 207);
+            this.Opis_label.Name = "Opis_label";
+            this.Opis_label.Size = new System.Drawing.Size(44, 17);
+            this.Opis_label.TabIndex = 5;
+            this.Opis_label.Tag = "Treść";
+            this.Opis_label.Text = "Treść";
+            // 
+            // razem_label
+            // 
+            this.razem_label.AutoSize = true;
+            this.razem_label.Location = new System.Drawing.Point(559, 309);
+            this.razem_label.Name = "razem_label";
+            this.razem_label.Size = new System.Drawing.Size(52, 17);
+            this.razem_label.TabIndex = 6;
+            this.razem_label.Text = "Razem";
+            // 
+            // cena_label
+            // 
+            this.cena_label.AutoSize = true;
+            this.cena_label.Location = new System.Drawing.Point(626, 225);
+            this.cena_label.Name = "cena_label";
+            this.cena_label.Size = new System.Drawing.Size(41, 17);
+            this.cena_label.TabIndex = 7;
+            this.cena_label.Text = "Cena";
+            // 
+            // lp_label
+            // 
+            this.lp_label.AutoSize = true;
+            this.lp_label.Location = new System.Drawing.Point(56, 207);
+            this.lp_label.Name = "lp_label";
+            this.lp_label.Size = new System.Drawing.Size(28, 17);
+            this.lp_label.TabIndex = 8;
+            this.lp_label.Text = "L.p";
+            // 
+            // dataWystawienia_label
+            // 
+            this.dataWystawienia_label.AutoSize = true;
+            this.dataWystawienia_label.Location = new System.Drawing.Point(56, 80);
+            this.dataWystawienia_label.Name = "dataWystawienia_label";
+            this.dataWystawienia_label.Size = new System.Drawing.Size(116, 17);
+            this.dataWystawienia_label.TabIndex = 9;
+            this.dataWystawienia_label.Text = "Data wystawienia";
+            // 
+            // Seria_label
+            // 
+            this.Seria_label.AutoSize = true;
+            this.Seria_label.Location = new System.Drawing.Point(56, 133);
+            this.Seria_label.Name = "Seria_label";
+            this.Seria_label.Size = new System.Drawing.Size(41, 17);
+            this.Seria_label.TabIndex = 10;
+            this.Seria_label.Text = "Seria";
+            // 
+            // Nabywca_label
+            // 
+            this.Nabywca_label.AutoSize = true;
+            this.Nabywca_label.Location = new System.Drawing.Point(56, 24);
+            this.Nabywca_label.Name = "Nabywca_label";
+            this.Nabywca_label.Size = new System.Drawing.Size(65, 17);
+            this.Nabywca_label.TabIndex = 11;
+            this.Nabywca_label.Text = "Nabywca";
+            // 
+            // terminPlatnosci_label
+            // 
+            this.terminPlatnosci_label.AutoSize = true;
+            this.terminPlatnosci_label.Location = new System.Drawing.Point(407, 80);
+            this.terminPlatnosci_label.Name = "terminPlatnosci_label";
+            this.terminPlatnosci_label.Size = new System.Drawing.Size(112, 17);
+            this.terminPlatnosci_label.TabIndex = 12;
+            this.terminPlatnosci_label.Text = "Termin płatności";
+            // 
+            // tresc_richTextBox
+            // 
+            this.tresc_richTextBox.Location = new System.Drawing.Point(105, 227);
+            this.tresc_richTextBox.Name = "tresc_richTextBox";
+            this.tresc_richTextBox.Size = new System.Drawing.Size(515, 65);
+            this.tresc_richTextBox.TabIndex = 13;
+            this.tresc_richTextBox.Text = "";
+            // 
+            // oplacono_textBox
+            // 
+            this.oplacono_textBox.Location = new System.Drawing.Point(486, 153);
+            this.oplacono_textBox.Name = "oplacono_textBox";
+            this.oplacono_textBox.Size = new System.Drawing.Size(100, 22);
+            this.oplacono_textBox.TabIndex = 14;
+            // 
+            // terminPlatnosci_comboBox
+            // 
+            this.terminPlatnosci_comboBox.FormattingEnabled = true;
+            this.terminPlatnosci_comboBox.Location = new System.Drawing.Point(407, 98);
+            this.terminPlatnosci_comboBox.Name = "terminPlatnosci_comboBox";
+            this.terminPlatnosci_comboBox.Size = new System.Drawing.Size(121, 24);
+            this.terminPlatnosci_comboBox.TabIndex = 15;
+            // 
+            // dodajPozycje_button
+            // 
+            this.dodajPozycje_button.Location = new System.Drawing.Point(51, 309);
+            this.dodajPozycje_button.Name = "dodajPozycje_button";
+            this.dodajPozycje_button.Size = new System.Drawing.Size(143, 31);
+            this.dodajPozycje_button.TabIndex = 17;
+            this.dodajPozycje_button.Text = "Nowa pozycja";
+            this.dodajPozycje_button.UseVisualStyleBackColor = true;
+            // 
+            // DataWystawienia_dateTimePicker
+            // 
+            this.DataWystawienia_dateTimePicker.Location = new System.Drawing.Point(51, 98);
+            this.DataWystawienia_dateTimePicker.Name = "DataWystawienia_dateTimePicker";
+            this.DataWystawienia_dateTimePicker.Size = new System.Drawing.Size(350, 22);
+            this.DataWystawienia_dateTimePicker.TabIndex = 18;
+            // 
+            // nabywca_comboBox
+            // 
+            this.nabywca_comboBox.FormattingEnabled = true;
+            this.nabywca_comboBox.Location = new System.Drawing.Point(51, 44);
+            this.nabywca_comboBox.Name = "nabywca_comboBox";
+            this.nabywca_comboBox.Size = new System.Drawing.Size(675, 24);
+            this.nabywca_comboBox.TabIndex = 19;
+            // 
+            // seria_comboBox
+            // 
+            this.seria_comboBox.FormattingEnabled = true;
+            this.seria_comboBox.Location = new System.Drawing.Point(51, 151);
+            this.seria_comboBox.Name = "seria_comboBox";
+            this.seria_comboBox.Size = new System.Drawing.Size(271, 24);
+            this.seria_comboBox.TabIndex = 20;
+            // 
+            // metodaPlatnosci_comboBox
+            // 
+            this.metodaPlatnosci_comboBox.FormattingEnabled = true;
+            this.metodaPlatnosci_comboBox.Location = new System.Drawing.Point(328, 151);
+            this.metodaPlatnosci_comboBox.Name = "metodaPlatnosci_comboBox";
+            this.metodaPlatnosci_comboBox.Size = new System.Drawing.Size(121, 24);
+            this.metodaPlatnosci_comboBox.TabIndex = 21;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(455, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 24);
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LPPozycjaNoty_richTextBox
+            // 
+            this.LPPozycjaNoty_richTextBox.Location = new System.Drawing.Point(51, 227);
+            this.LPPozycjaNoty_richTextBox.Name = "LPPozycjaNoty_richTextBox";
+            this.LPPozycjaNoty_richTextBox.Size = new System.Drawing.Size(48, 65);
+            this.LPPozycjaNoty_richTextBox.TabIndex = 24;
+            this.LPPozycjaNoty_richTextBox.Text = "";
+            // 
+            // pozycjaNoty_richTextBox
+            // 
+            this.pozycjaNoty_richTextBox.Location = new System.Drawing.Point(626, 245);
+            this.pozycjaNoty_richTextBox.Name = "pozycjaNoty_richTextBox";
+            this.pozycjaNoty_richTextBox.Size = new System.Drawing.Size(82, 47);
+            this.pozycjaNoty_richTextBox.TabIndex = 25;
+            this.pozycjaNoty_richTextBox.Text = "";
+            // 
+            // razem_richTextBox
+            // 
+            this.razem_richTextBox.Location = new System.Drawing.Point(626, 298);
+            this.razem_richTextBox.Name = "razem_richTextBox";
+            this.razem_richTextBox.Size = new System.Drawing.Size(82, 47);
+            this.razem_richTextBox.TabIndex = 26;
+            this.razem_richTextBox.Text = "";
+            // 
+            // anuluj_button
+            // 
+            this.anuluj_button.Location = new System.Drawing.Point(562, 366);
+            this.anuluj_button.Name = "anuluj_button";
+            this.anuluj_button.Size = new System.Drawing.Size(85, 32);
+            this.anuluj_button.TabIndex = 28;
+            this.anuluj_button.Text = "Anuluj";
+            this.anuluj_button.UseVisualStyleBackColor = true;
+            // 
+            // zapisz_button
+            // 
+            this.zapisz_button.Location = new System.Drawing.Point(653, 366);
+            this.zapisz_button.Name = "zapisz_button";
+            this.zapisz_button.Size = new System.Drawing.Size(92, 32);
+            this.zapisz_button.TabIndex = 29;
+            this.zapisz_button.Text = "Zapisz ..";
+            this.zapisz_button.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(720, 258);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 24);
+            this.pictureBox2.TabIndex = 30;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(720, 309);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(25, 24);
+            this.pictureBox3.TabIndex = 31;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(720, 194);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(25, 24);
+            this.pictureBox4.TabIndex = 32;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(592, 151);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(25, 24);
+            this.pictureBox5.TabIndex = 33;
+            this.pictureBox5.TabStop = false;
+            // 
+            // załącznik_button
+            // 
+            this.załącznik_button.Location = new System.Drawing.Point(51, 366);
+            this.załącznik_button.Name = "załącznik_button";
+            this.załącznik_button.Size = new System.Drawing.Size(143, 32);
+            this.załącznik_button.TabIndex = 34;
+            this.załącznik_button.Text = "Załącz plik";
+            this.załącznik_button.UseVisualStyleBackColor = true;
             // 
             // NowaNotaKsiegowa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.załącznik_button);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.zapisz_button);
+            this.Controls.Add(this.anuluj_button);
+            this.Controls.Add(this.razem_richTextBox);
+            this.Controls.Add(this.pozycjaNoty_richTextBox);
+            this.Controls.Add(this.LPPozycjaNoty_richTextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metodaPlatnosci_comboBox);
+            this.Controls.Add(this.seria_comboBox);
+            this.Controls.Add(this.nabywca_comboBox);
+            this.Controls.Add(this.DataWystawienia_dateTimePicker);
+            this.Controls.Add(this.dodajPozycje_button);
+            this.Controls.Add(this.terminPlatnosci_comboBox);
+            this.Controls.Add(this.oplacono_textBox);
+            this.Controls.Add(this.tresc_richTextBox);
+            this.Controls.Add(this.terminPlatnosci_label);
+            this.Controls.Add(this.Nabywca_label);
+            this.Controls.Add(this.Seria_label);
+            this.Controls.Add(this.dataWystawienia_label);
+            this.Controls.Add(this.lp_label);
+            this.Controls.Add(this.cena_label);
+            this.Controls.Add(this.razem_label);
+            this.Controls.Add(this.Opis_label);
+            this.Controls.Add(this.metodaPlatnosci_label);
+            this.Controls.Add(this.oplacono_label);
             this.Name = "NowaNotaKsiegowa";
             this.Text = "NowaNotaKsiegowa";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label oplacono_label;
+        private System.Windows.Forms.Label metodaPlatnosci_label;
+        private System.Windows.Forms.Label Opis_label;
+        private System.Windows.Forms.Label razem_label;
+        private System.Windows.Forms.Label cena_label;
+        private System.Windows.Forms.Label lp_label;
+        private System.Windows.Forms.Label dataWystawienia_label;
+        private System.Windows.Forms.Label Seria_label;
+        private System.Windows.Forms.Label Nabywca_label;
+        private System.Windows.Forms.Label terminPlatnosci_label;
+        private System.Windows.Forms.RichTextBox tresc_richTextBox;
+        private System.Windows.Forms.TextBox oplacono_textBox;
+        private System.Windows.Forms.ComboBox terminPlatnosci_comboBox;
+        private System.Windows.Forms.Button dodajPozycje_button;
+        private System.Windows.Forms.DateTimePicker DataWystawienia_dateTimePicker;
+        private System.Windows.Forms.ComboBox nabywca_comboBox;
+        private System.Windows.Forms.ComboBox seria_comboBox;
+        private System.Windows.Forms.ComboBox metodaPlatnosci_comboBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox LPPozycjaNoty_richTextBox;
+        private System.Windows.Forms.RichTextBox pozycjaNoty_richTextBox;
+        private System.Windows.Forms.RichTextBox razem_richTextBox;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button anuluj_button;
+        private System.Windows.Forms.Button zapisz_button;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button załącznik_button;
     }
 }
